e35cbb7556bbf1688c5e034ae7368c6164b36768
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-02-08 21:09:03 -0500

    added string and display of names

--- Student Database/Program.cs
+++ Student Database/Program.cs
@@ -2,9 +2,15 @@
 
 //Create 3 arrays. One with name, one with hometown and one with fav food
 
-string[]name = new string[] {"Chris Cornell", "Curt Cobain", "Chris Robinson", "Robin Zander", "Stephen Tyler"};
+string[]names = new string[] {"Chris Cornell", "Curt Cobain", "Chris Robinson", "Robin Zander", "Stephen Tyler"};
 
 string[]hometown = new string[] {"Seattle", "Atlanta","Aberdeen","Beloit", "Yonkers" };
 
 string[]favoriteFood = new string[] {"Oysters","Prawn Salad","Tacos","Eggs","Pancakes"};
 
+Console.WriteLine("Welcome!\n Which student would you like to learn more about? Enter a number 1-5: ");
+int indexCounter = 0;
+foreach(string name in names)
+{
+    Console.WriteLine($"name{indexCounter + 1}");
+}

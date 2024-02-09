9ae780384081aaf409a114132e0ac52d51e96ec6
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-02-08 20:45:17 -0500

    Add project files.

---
+++ Student Database/Program.cs
@@ -0,0 +1,10 @@
+﻿//Student Database
+
+//Create 3 arrays. One with name, one with hometown and one with fav food
+
+string[]name = new string[] {"Chris Cornell", "Curt Cobain", "Chris Robinson", "Robin Zander", "Stephen Tyler"};
+
+string[]hometown = new string[] {"Seattle", "Atlanta","Aberdeen","Beloit", "Yonkers" };
+
+string[]favoriteFood = new string[] {"Oysters","Prawn Salad","Tacos","Eggs","Pancakes"};
+
